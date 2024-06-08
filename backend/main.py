508df047ab7d523f4500d498b93b22d9ab01552e
FrancoIvanFerreyra <fiferreyra@fi.uba.ar>
2024-06-08 20:15:25 -0300

    Adding test table creation in database

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from models import db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -10,5 +11,8 @@ def hello_world():
     return "<p>Hello world</p>"
 
 if __name__ == "__main__":
+    db.init_app(app)
+    with app.app_context():
+      db.create_all()
     app.run(port=8000)
 
