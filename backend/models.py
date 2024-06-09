5c620f2aa4ffa1201ce1aa24d6017ab8d7c05bdd
FrancoIvanFerreyra <fiferreyra@fi.uba.ar>
2024-06-09 17:11:49 -0300

    Added product table class, test class removed

--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Test(db.Model):
-     __tablename__ = "tablaprueba"
-     id = db.Column(db.Integer, primary_key=True)
-     nombre = db.Column(db.String(80), nullable=False)
-     email = db.Column(db.String(150), unique=True)
+class Product(db.Model):
+     __tablename__ = "products"
+     id = db.Column(db.Integer, primary_key=True, )
+     name = db.Column(db.String(80), nullable=False)
+     description = db.Column(db.String(150))
+     weight = db.Column(db.Float)
+     dueDate = db.Column(db.DateTime)
+     price = db.Column(db.Float, nullable=False)
+     
