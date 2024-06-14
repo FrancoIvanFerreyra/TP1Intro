ff461eacad1177c508cdcbfb0c296c8964bd1055
FrancoIvanFerreyra <fiferreyra@fi.uba.ar>
2024-06-14 19:12:38 -0300

    Add client model for database

--- backend/models.py
+++ backend/models.py
@@ -8,4 +8,12 @@ class Product(db.Model):
      name = db.Column(db.String(80), nullable=False)
      description = db.Column(db.String(150))
      price = db.Column(db.Float, nullable=False)
+
+class Client(db.Model):
+     __tablename__ = "clients"
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(80), nullable=False)
+     surname = db.Column(db.String(80), nullable=False)
+     email = db.Column(db.String(80), unique=True, nullable=False)
+     phoneNumber = db.Column(db.String(80), nullable=False)
      
