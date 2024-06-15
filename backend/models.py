4c29f2a78c7b299f268355ab3c2af4afba6e1f2b
Ignacio Berango <ignacioberango@gmail.com>
2024-06-14 23:05:07 -0300

    Added 'Category' table model

--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,7 @@ class Product(db.Model):
      description = db.Column(db.String(150))
      price = db.Column(db.Float, nullable=False)
      
+class Category(db.Model):
+     __tablename__ = "categories"
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(50), nullable=False)
