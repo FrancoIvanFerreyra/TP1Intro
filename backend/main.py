b2e36156bdf626e36c597fbcc817d35f53a4e380
Ignacio Berango <ignacioberango@gmail.com>
2024-06-19 11:39:41 -0300

    flask-cors added

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from models import db, Product, Category
 
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
