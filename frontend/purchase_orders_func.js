55ac4d751f0e4df5f1f6751fb284f66bd6e604f6
FrancoIvanFerreyra <fiferreyra@fi.uba.ar>
2024-07-11 16:09:10 -0300

    Prevent invalid characters on input

--- frontend/purchase_orders_func.js
+++ frontend/purchase_orders_func.js
@@ -1,7 +1,20 @@
-alert("Hello world!");
 
 function send_order_data()
 {
     const data = document.getElementById("order_id");
-    console.log(data.value);
+    console.log(data.value.toString());
+
 }
+
+const invalid_characters = ["e", "+", "-"];
+document.querySelector(".validate").addEventListener("keypress", function(event)
+{
+    if(invalid_characters.includes(event.key))
+    {
+        event.preventDefault();
+    }
+});
+
+
+
+
