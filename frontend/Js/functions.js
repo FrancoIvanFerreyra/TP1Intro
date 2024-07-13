2ffd8e5212429123fdf442c44ad2d52e1e8ecef9
Ignacio Berango <ignacioberango@gmail.com>
2024-07-13 16:45:04 -0300

    add_purchase_order function added

--- frontend/Js/functions.js
+++ frontend/Js/functions.js
@@ -7,7 +7,6 @@ function send_form(event){
     const surname = Data.get("surname")
     const email = Data.get("email")
     const phone_number = Data.get("phone_number")
-    //const payment_method = Data.get("payment_method")
 
     fetch("http://localhost:5000/clients", {
         method: "POST",
@@ -19,21 +18,47 @@ function send_form(event){
             surname: surname,
             email: email,
             phone_number: phone_number,
-            //payment_method: payment_method,
         })
     })
         .then((response) => response.json())
-        .then(handle_response)
+        .then((data) => handle_response(data, event))
         .catch(handle_error)
 }
 
-function handle_response(data){
-    if(data == "Client saved correctly"){
+function handle_response(data, event){
+    if(data[0] == "Client saved correctly"){
         alert("¡La compra se ha realizado con éxito!")
         window.location.href = '/'
+
+        add_purchase_order(data[1], event)
     }else{
         alert("Error: la compra ha fallado.")
     }
+
+}
+
+function add_purchase_order(client_id, event){
+    
+    const Data = new FormData(event.target)
+    const payment_method = Data.get("payment_method")
+
+    fetch("http://localhost:5000/purchase_orders", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            client_id: client_id,
+            payment_method: payment_method,
+        })
+    })
+        .then((response) => response.json())
+        .then(handle_response_purchase_order)
+        .catch(handle_error)
+}
+
+function handle_response_purchase_order(data){
+    console.log(data)
 }
 
 function buttons_categorie(category_list){
